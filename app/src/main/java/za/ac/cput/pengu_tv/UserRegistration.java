a0c4c738b38c43675dbba8cefdfce949ca913dae
MaryBethA <79981441+MaryBethA@users.noreply.github.com>
2022-08-30 22:02:09 +0200

    Splash Screen

--- app/src/main/java/za/ac/cput/pengu_tv/UserRegistration.java
+++ app/src/main/java/za/ac/cput/pengu_tv/UserRegistration.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,6 +23,9 @@ public class UserRegistration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_user_registration);
 
 
